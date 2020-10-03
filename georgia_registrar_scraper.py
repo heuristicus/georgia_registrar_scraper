1a81a099398c7da744bb6d2a012dcee99f1dab15
Michal Staniaszek <m.staniaszek@gmail.com>
2020-10-03 16:18:01 +0100

    Write to file for each county rather than all at the end

--- georgia_registrar_scraper.py
+++ georgia_registrar_scraper.py
@@ -9,36 +9,34 @@ browser.get("https://elections.sos.ga.gov/Elections/countyregistrars.do")
 
 county_dropdown = Select(browser.find_element_by_id("idTown"))
 
-registrars = {}
-
-for i in range(0, len(county_dropdown.options)):
-    county_dropdown = Select(browser.find_element_by_id("idTown"))
-    county_dropdown.select_by_index(i)
-    county_name = county_dropdown.first_selected_option.text
-    # Since we will be going back and forth on the page need to refresh this each time
-    select_button = browser.find_element_by_name("SubmitCounty")
-    select_button.click()
-    back_button = browser.find_element_by_name("SubmitBack")
-    # The first element tbody is the "elections division" header
-    registrar_data = browser.find_elements_by_tag_name("tbody")[1]
-
-    # Strip extraneous whitespace from the text, then remove the first line "COUNTY BOARD OF REGISTRARS OFFICES"
-    registrars[str.capitalize(county_name)] = "\n".join(registrar_data.text.strip().splitlines()[1:])
-
-    back_button.click()
-
 with open("georgia_registrars.txt", 'w') as f:
     # Use this regex to split the registrar data and put it into the file in a more easily readable format.
     # The different contact detail lines end in a colon.
     endcolon = re.compile("(.*:)$\n", flags=re.M)
-    for county in sorted(registrars):
-        f.write(county + " County\n")
-        split_details = endcolon.split(registrars[county])
+    for i in range(0, len(county_dropdown.options)):
+        county_dropdown = Select(browser.find_element_by_id("idTown"))
+        county_dropdown.select_by_index(i)
+        county_name = str.capitalize(county_dropdown.first_selected_option.text)
+        # Since we will be going back and forth on the page need to refresh this each time
+        select_button = browser.find_element_by_name("SubmitCounty")
+        select_button.click()
+        back_button = browser.find_element_by_name("SubmitBack")
+        # The first element tbody is the "elections division" header
+        registrar_data = browser.find_elements_by_tag_name("tbody")[1]
+
+        # Strip extraneous whitespace from the text, then remove the first line "COUNTY BOARD OF REGISTRARS OFFICES"
+        # Rejoin the details to allow regex processing
+        details = "\n".join(registrar_data.text.strip().splitlines()[1:])
+
+        f.write(county_name + " County\n")
+        split_details = endcolon.split(details)
         # The split will have either 5 or 7 elements. There can be a physical address as well as a mailing address,
         # but there is always at least one of these, and there is always contact information.
         f.write(split_details[0] + "\n")  # the name of the registrar
         # Skip ahead by two each loop as the heading and data are paired
-        for i in range(1, len(split_details[1:]), 2):
-            f.write(split_details[i] + "\n" + split_details[i + 1] + "\n")
+        for ind in range(1, len(split_details[1:]), 2):
+            f.write(split_details[ind] + "\n" + split_details[ind + 1] + "\n")
 
         f.write("\n")
+
+        back_button.click()
